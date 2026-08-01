722901e24f1bd8c079002bd9e941b7f937eeea89
Shekhawat <rajputsahilshekhawat@gmail.com>
2026-08-01 18:11:30 +0530

    Force IPv4 for Gmail SMTP

--- server/config/mail.js
+++ server/config/mail.js
@@ -2,17 +2,14 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // true sirf port 465 ke liye hota hai
+  port: 465,
+  secure: true,
+  family: 4, // Force IPv4
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-
-  connectionTimeout: 30000,
-  greetingTimeout: 30000,
-  socketTimeout: 30000,
 });
 
 export default transporter;
